416f2f89bec4aa907be1dbe3e58ccf56a5423c8c
tuong <110910077+tuongkhtn@users.noreply.github.com>
2024-08-21 18:11:32 +0700

    Update name model resnet

--- backend/src/model/resnet.py
+++ backend/src/model/resnet.py
@@ -1,19 +1,19 @@
 import torch.nn as nn
 from torchvision import models
 
-def create_resnet(n_classes: int = 2, model_name: str = 'resnet18', load_pretrained: bool = False):
-    if model_name == 'resnet18':
+def create_resnet(n_classes: int = 2, model_name: str = 'resnet_18', load_pretrained: bool = False):
+    if model_name == 'resnet_18':
         if load_pretrained:
             model = models.resnet18(weights=models.ResNet18_Weights.IMAGENET1K_V1)
         else:
             model = models.resnet18()
-    elif model_name == 'resnet34':
+    elif model_name == 'resnet_34':
         if load_pretrained:
             model = models.resnet34(weights=models.ResNet34_Weights.IMAGENET1K_V1)
         else:
             model = models.resnet34()
     else:
-        raise ValueError(f'Invalid model name: [resnet18, resnet34]')
+        raise ValueError(f'Invalid model name: [resnet_18, resnet_34]')
 
     in_features = model.fc.in_features
     model.fc = nn.Linear(in_features, n_classes)
